baa4d1794a414a0d5bde260c2f251105cebee4ea
Joey Comeau <comeau.joey@gmail.com>
2016-04-02 18:15:40 -0700

    side bar: update

--- js/side_bar.js
+++ js/side_bar.js
@@ -35,9 +35,22 @@ function changeEventInfo(e, t, n) {
 
 function setEventDetails(e, t) {
     $.getJSON(e, function(e) {
-        $(".event-info--title").html(e.title), $(".js-event-info-date").html(e.longDates), $(".js-event-info-time").html(e.language), $(".js-event-info-venue").html(e.category), $(".js-event-info-fb").attr("href", "https://www.facebook.com/sharer/sharer.php?u=" + encodeURI(e.url)), $(".js-event-info-twitter").attr("href", "https://twitter.com/home?status=" + encodeURI(e.title + " " + e.url)), null != e.more_info ? $(".js-event-info-wrapper").html('<a href="' + e.more_info + '" target="_blank">More Info</a>') : $(".js-event-info-wrapper").html(""), $(".event-info .body-copy").html("<p>" + e.content + "</p>");
+        $(".event-info--title").html(e.title), 
+        $(".js-event-info-date").html(e.longDates), 
+        $(".js-event-info-time").html(e.language), 
+        $(".js-event-info-venue").html(e.category), 
+        $(".js-event-info-fb").attr("href", "https://www.facebook.com/sharer/sharer.php?u=" + encodeURI(e.url)), 
+        $(".js-event-info-twitter").attr("href", "https://twitter.com/home?status=" + encodeURI(e.title + " " + e.url)), 
+        null != e.more_info ? $(".js-event-info-wrapper").html('<a href="' + e.more_info + '" target="_blank">More Info</a>') : $(".js-event-info-wrapper").html(""), 
+        $(".event-info .body-copy").html("<p>" + e.content + "</p>");
         var n;
-        n = ".event-background-color { background: " + e.color_scheme.color_1 + "; fill: " + e.color_scheme.color_1 + ";}", n += n += ".event-primary-color { color: " + e.color_scheme.color_2 + "; fill: " + e.color_scheme.color_2 + ";}", n += ".slash { stroke: " + e.color_scheme.color_3 + ";}", n += ".event-secondary-color { color: " + e.color_scheme.color_3 + "; fill: " + e.color_scheme.color_3 + ";}", n += ".event-info .body-copy a, .event-info .meta-data a { color: " + e.color_scheme.color_3 + ";}", n += ".event-info .js-event-info-wrapper a { color: " + e.color_scheme.color_3 + ";}", n += ".event-info .event-info--share a:hover { color: " + e.color_scheme.color_3 + ";}", $("#js-event-color-scheme").remove(), $('<style id="js-event-color-scheme" type="text/css">' + n + "</style>").appendTo("head"), Sidebar.animOpen(), t && t(e)
+        n = ".event-background-color { background: " + e.color_scheme.color_1 + "; fill: " + e.color_scheme.color_1 + ";}", 
+        n += n += ".event-primary-color { color: " + e.color_scheme.color_2 + "; fill: " + e.color_scheme.color_2 + ";}", 
+        n += ".slash { stroke: " + e.color_scheme.color_3 + ";}", 
+        n += ".event-secondary-color { color: " + e.color_scheme.color_3 + "; fill: " + e.color_scheme.color_3 + ";}", 
+        n += ".event-info .body-copy a, .event-info .meta-data a { color: " + e.color_scheme.color_3 + ";}", 
+        n += ".event-info .js-event-info-wrapper a { color: " + e.color_scheme.color_3 + ";}", 
+        n += ".event-info .event-info--share a:hover { color: " + e.color_scheme.color_3 + ";}", $("#js-event-color-scheme").remove(), $('<style id="js-event-color-scheme" type="text/css">' + n + "</style>").appendTo("head"), Sidebar.animOpen(), t && t(e)
     })
 }
 
@@ -162,7 +175,7 @@ $(function() {
         n = $(".filter");
     $(".event.event--active").length && changeEventInfo($(".event.event--active"), e), t.on("click touch", function(e) {
         e.preventDefault(), stateHandler.push({
-            url: "/projects",
+            url: "/",
             title: "Joey Comeau",
             eventId: null
         }), Sidebar.animClose()
